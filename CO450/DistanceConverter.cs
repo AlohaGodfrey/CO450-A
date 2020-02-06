101047eefacae012548c6a2fedac542f77b9d5c0
Godfrey <Immanuelkrave3@hotmail.co.uk>
2020-02-06 23:19:37 +0000

    New 4.1 Completed

---
+++ CO450/DistanceConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CO450
+{
+    class DistanceConverter
+    {
+        public int toFeet(int miles)
+        {
+            return miles*1760*3;
+        }
+
+        public int toMiles(int feet)
+        {
+            return feet / 1760 / 3;
+        }
+
+        public int GetInput(string Unit)
+        {
+            string input;
+            int value;
+
+            Console.Write("Enter distance in "+ Unit +": ");
+            input = Console.ReadLine();
+            value = Convert.ToInt32(input);
+
+            return value;
+        }
+
+        public void DisplayChoice()
+        {
+            Console.WriteLine("Godfrey's Distance Converter");
+            Console.WriteLine("=============================");
+            Console.WriteLine("1. Convert miles to feet");
+            Console.WriteLine("2. Convert feet to miles");
+            Console.WriteLine("3. Quit test");
+
+        }
+    }
+}
--- CO450/Program.cs
+++ CO450/Program.cs
@@ -10,6 +10,47 @@ namespace CO450
     {
         static void Main(string[] args)
         {
+            int miles, feet, choice;
+            string input;
+
+            DistanceConverter converter = new DistanceConverter();
+
+            do
+            {
+                
+                converter.DisplayChoice();
+                Console.Write("option: ");
+                input = Console.ReadLine();
+                choice = Convert.ToInt32(input);
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Clear();
+                        miles = converter.GetInput("miles");
+                        feet = converter.toFeet(miles);
+                        Console.WriteLine(miles + " miles in feet: " + feet);
+                        Console.WriteLine();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        feet = converter.GetInput("feet");
+                        miles = converter.toMiles(feet);
+                        Console.WriteLine(feet + " feet is equivalent to " + miles + " miles.");
+                        Console.WriteLine();
+                        break;
+                    case 3:
+                        Console.WriteLine("...Exiting Program");
+                        break;
+                    default:
+                        Console.WriteLine("Select an Option from the Menu Above!");
+                        Console.WriteLine();
+                        break;
+                }
+            } while (choice != 3);
+
+            Console.ReadLine();
+           
         }
     }
 }
